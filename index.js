a8077190f8dde94131afa7e35f803ba008727fe2
Carlos <garcia.edu21@hotmail.com>
2021-04-15 15:41:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// 1) is an equation whose multiplied results will equal 62
+//  randm
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+
+//   2) generates a random integer greater than 0
+// mod
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+//   3) is an equation that calculates a remainder that is equal to 4
+// max
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+
+//   4) will return 20 as the highest number in the set
+
+
+let max = Math.max( 5, 10, 15, 20 );
+
